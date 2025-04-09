1031736846146262704d2e8b116def9b57b7638e
n-valla <nikhilmsr@gmail.com>
2025-04-08 22:01:15 -0400

    created functionality for user.java and photo.java

--- src/Photo.java
+++ src/Photo.java
@@ -1,4 +1,86 @@
 package src;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.swing.text.html.HTML.Tag;
+
 public class Photo {
+
+    private String filePath;
+    private String caption;
+    private LocalDateTime dateTaken;
+    private List<Tag> tags;
+
+    public Photo(String filePath, LocalDateTime dateTaken) {
+        this.filePath = filePath;
+        this.dateTaken = dateTaken;
+        this.caption = "";
+        this.tags = new ArrayList<>();
+    }
+
+    public String getFilePath() 
+    {
+        return filePath;
+    }
+
+    public String getCaption() 
+    {
+        return caption;
+    }
+
+    public LocalDateTime getDateTaken() 
+    {
+        return dateTaken;
+    }
+
+    public List<Tag> getTags() 
+    {
+        return tags;
+    }
+
+    public void setCaption(String caption) 
+    {
+        this.caption = caption;
+    }
+
+    public void addTag(Tag tag) 
+    {
+        if (!tags.contains(tag)) 
+        {
+            tags.add(tag);
+        }
+    }
+
+    public void removeTag(Tag tag) 
+    {
+        tags.remove(tag);
+    }
+
+    public boolean hasTag(Tag tag) 
+    {
+        return tags.contains(tag);
+    }
+
+ 
+    public boolean Equals(Object o) 
+    {
+        if (this == o) return true;
+        if (!(o instanceof Photo)) return false;
+        
+        Photo photo = (Photo) o;
+        return filePath.equals(photo.filePath);
+    }
+
+    public int hash_Code() 
+    {
+        return Objects.hash(filePath);
+    }
+
+    public String to_String() 
+    {
+        return "Photo: " + filePath + ", caption: " + caption;
+    }
     
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,62 @@
 package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     
+    private String username;
+    private List<Album> albums;
+
+    public User(String username)
+    {
+        this.username = username;
+        this.albums = new ArrayList<>();
+    }
+
+    public String getUsername() 
+    {
+        return username;
+    }
+    
+    public List<Album> getAlbums() 
+    {
+        return albums;
+    }
+
+    public void addAlbum(Album album) 
+    {
+        if (!albums.contains(album)) 
+        {
+            albums.add(album);
+        }
+    }
+
+    public void removeAlbum(Album album) 
+    {
+        albums.remove(album);
+    }
+
+
+    public Album getAlbumByName(String name) 
+    {
+        for (Album album : albums) 
+        {
+            if (album.getName().equalsIgnoreCase(name)) 
+            {
+                return album;
+            }
+        }
+        return null;
+    }
+
+    public boolean hasAlbum(String name) 
+    {
+        return getAlbumByName(name) != null;
+    }
+
+    public String to_String() 
+    {
+        return "User: " + username + " (Albums: " + albums.size() + ")";
+    }
 }
